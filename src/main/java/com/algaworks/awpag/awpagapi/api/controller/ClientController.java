4f7877bb03d19d629d9e3ed3942f069228492ae8
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-03-18 16:42:18 -0300

    feat: creating Client Model

---
+++ src/main/java/com/algaworks/awpag/awpagapi/api/controller/ClientController.java
@@ -0,0 +1,28 @@
+package com.algaworks.awpag.awpagapi.api.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.awpag.awpagapi.domain.model.Client;
+
+@RestController
+public class ClientController {
+  @GetMapping("/clients")
+  public List<Client> list() {
+    Client client1 = new Client();
+    client1.setId(1);
+    client1.setName("John Doe");
+    client1.setEmail("jdoe@ex.com");
+    client1.setTelephone("(11) 99999-9999");
+    Client client2 = new Client();
+    client1.setId(2);
+    client1.setName("Mary Doe");
+    client1.setEmail("mdoe@ex.com");
+    client1.setTelephone("(41) 5353-9999");
+
+    return Arrays.asList(client1, client2);
+  }
+}
---
+++ src/main/java/com/algaworks/awpag/awpagapi/domain/model/Client.java
@@ -0,0 +1,16 @@
+package com.algaworks.awpag.awpagapi.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// Create getters and setters for all properties
+@Getter
+@Setter
+public class Client {
+
+  private int id;
+  private String name;
+  private String email;
+  private String telephone;
+
+}
