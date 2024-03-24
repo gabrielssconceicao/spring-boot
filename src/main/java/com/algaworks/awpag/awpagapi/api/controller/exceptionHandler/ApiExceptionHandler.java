6d65a951433b42e8771c74b4a82c4b3d478a7113
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-03-24 10:26:50 -0300

    feat: Exception -> returning field errors

--- src/main/java/com/algaworks/awpag/awpagapi/api/controller/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/awpag/awpagapi/api/controller/exceptionHandler/ApiExceptionHandler.java
@@ -1,13 +1,45 @@
 package com.algaworks.awpag.awpagapi.api.controller.exceptionHandler;
 
 import com.algaworks.awpag.awpagapi.domain.exception.BusinessException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
+import java.util.stream.Collectors;
 
 // é  capaz de capturar as exceçoes de todos os controllers
 @RestControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers,
+            HttpStatusCode status, WebRequest request)
+    {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Um ou mais campos estão inválidos");
+        problemDetail.setType(URI.create("https://api.com/erros/campos-invalidos"));
+
+        var fields = ex.getBindingResult().getAllErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        error -> ((FieldError)error).getField(),
+                        DefaultMessageSourceResolvable::getDefaultMessage
+                ));
+        problemDetail.setProperty("fields",fields);
+
+        return super.handleExceptionInternal(ex,problemDetail, headers, status, request);
+    }
+
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<String> capture(BusinessException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
