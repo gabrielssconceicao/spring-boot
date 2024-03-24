f7982799ca74e8d15df1e8a0c7bce4a1aa368a76
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-03-24 13:46:37 -0300

    feat: parcelamtoAssembler -> mapper

---
+++ src/main/java/com/algaworks/awpag/awpagapi/api/assembler/ParcelamentoAssembler.java
@@ -0,0 +1,27 @@
+package com.algaworks.awpag.awpagapi.api.assembler;
+
+import com.algaworks.awpag.awpagapi.api.model.ParcelamentoModel;
+import com.algaworks.awpag.awpagapi.domain.model.Parcelamento;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Component
+public class ParcelamentoAssembler {
+
+    private final ModelMapper modelMapper;
+
+    public ParcelamentoModel toModel(Parcelamento parcelamento) {
+        return modelMapper.map(parcelamento, ParcelamentoModel.class);
+    }
+
+    public List<ParcelamentoModel> toCollectionModel(List<Parcelamento> parcelamentos) {
+        return parcelamentos
+                .stream()
+                .map(this::toModel)
+                .toList();
+    }
+}
--- src/main/java/com/algaworks/awpag/awpagapi/api/controller/ParcelamentoController.java
+++ src/main/java/com/algaworks/awpag/awpagapi/api/controller/ParcelamentoController.java
@@ -2,13 +2,13 @@ package com.algaworks.awpag.awpagapi.api.controller;
 
 
 
+import com.algaworks.awpag.awpagapi.api.assembler.ParcelamentoAssembler;
 import com.algaworks.awpag.awpagapi.api.model.ParcelamentoModel;
 import com.algaworks.awpag.awpagapi.domain.model.Parcelamento;
 import com.algaworks.awpag.awpagapi.domain.repositoriy.IParcelamentoRepository;
 import com.algaworks.awpag.awpagapi.domain.service.ParcelamentoService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,26 +21,27 @@ import java.util.List;
 public class ParcelamentoController {
     private final IParcelamentoRepository parcelamentoRepository;
     private final ParcelamentoService parcelamentoService;
-    private final ModelMapper modelMapper;
+    private final ParcelamentoAssembler parcelamentoAssembler;
 
     @GetMapping
-    public List<Parcelamento> list() {
-        return parcelamentoRepository.findAll();
+    public List<ParcelamentoModel> list() {
+        return parcelamentoAssembler.toCollectionModel( parcelamentoRepository.findAll());
     }
 
     @GetMapping("/{parcelamentoId}")
     public ResponseEntity<ParcelamentoModel> search(@PathVariable Long parcelamentoId) {
         return parcelamentoRepository
                 .findById(parcelamentoId)
-                .map(parcelamento -> modelMapper.map(parcelamento,ParcelamentoModel.class))
+                .map(parcelamentoAssembler::toModel)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Parcelamento create(@Valid @RequestBody Parcelamento parcelamento) {
-        return parcelamentoService.create(parcelamento);
+    public ParcelamentoModel create(@Valid @RequestBody Parcelamento parcelamento) {
+        Parcelamento parcelamentoCadastrado= parcelamentoService.create(parcelamento);
+        return parcelamentoAssembler.toModel(parcelamentoCadastrado);
     }
 
 }
